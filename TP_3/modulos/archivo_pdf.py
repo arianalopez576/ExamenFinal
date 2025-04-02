52493c5e4a1679d534c193d2be14dfb9e84458a7
Ariana <arianalopez576@gmail.com>
2025-04-02 18:02:52 -0300

    archipdf

--- TP_3/modulos/archivo_pdf.py
+++ TP_3/modulos/archivo_pdf.py
@@ -12,36 +12,40 @@ class Archivo_PDF(Archivo_Informe):
     def _crear_archivo(self, p_archivo): 
         p_archivo.drawString(100, 750, 'Datos del programa')
 
-    def escribir_archivo(self, p_nom_archivo, p_ps, p_epoca, p_lista_inteligencia, p_inteligencia_prom, p_desviacion_estandar):
+    def escribir_archivo(self, p_nom_archivo, p_parametros, p_datos):
         archivo = canvas.Canvas(p_nom_archivo)
+        #p_datos es una lista que contiene 
+            # [0] epoca
+            # [1] lista inteligencia
+            # [2] inteligencia promedio
+            # [3] desviacion estandar 
         self._crear_archivo(archivo)  
-        self.crear_histograma(p_lista_inteligencia)
+        self.crear_histograma(p_datos[1])
         
         archivo.drawString(100, 720, "Fecha y hora actuales:")
         archivo.drawString(100, 700, str(self.obtener_fecha_hora()))
         
         y_position = 660
         
-        parametros = Parametros_de_Simulacion()
-        texto = str(parametros.dic_parametros)
+        texto = str(p_parametros.dic_parametros)
         archivo.drawString(100, 680, "Parametros de la simulacion:")
         archivo.drawString(100, y_position, texto)
         
         archivo.setFont("Helvetica", 12)
         archivo.drawString(100, y_position -20, "Época:")
-        archivo.drawString(100, y_position - 40, str(p_epoca))
+        archivo.drawString(100, y_position - 40, str(p_datos[0]))
         
         archivo.drawString(100, y_position - 60, "Inteligencia:")
-        archivo.drawString(100, y_position - 80, str(p_lista_inteligencia))
+        archivo.drawString(100, y_position - 80, str(p_datos[1]))
         
         archivo.drawString(100, y_position - 100, "Inteligencia promedio:")
-        archivo.drawString(100, y_position - 120, str(p_inteligencia_prom))  
+        archivo.drawString(100, y_position - 120, str(p_datos[2]))  
         
-        archivo.drawString(100, y_position - 100, "Desviacion estandar:")
-        archivo.drawString(100, y_position - 120, str(p_desviacion_estandar))  
+        archivo.drawString(100, y_position - 140, "Desviacion estandar:")
+        archivo.drawString(100, y_position - 160, str(p_datos[3]))  
         archivo.showPage()
         
-        archivo.drawString(100, 425, "Histograma con datos de la inteligencia de los microorganismos:")
+        archivo.drawString(100, 720, "Histograma con datos de la inteligencia de los microorganismos:")
         archivo.drawImage('grafico.png', 100, 400, width=400, height=300) 
         archivo.showPage()  
         
@@ -51,5 +55,5 @@ class Archivo_PDF(Archivo_Informe):
 if __name__ == '__main__':
     pdf = Archivo_PDF()
     ps = Parametros_de_Simulacion()
-    lista_intel = [0, 5, 4, 2, 0, 0, 0]
-    pdf.escribir_archivo('archi_prueba.pdf', 'Datos del programa', ps, '10', lista_intel, 20)
+    p_datos = [10, [5,3,2,1,1], 2.5, 0.9]
+    pdf.escribir_archivo('archi_prueba.pdf', ps, p_datos)
